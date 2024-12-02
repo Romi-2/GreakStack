c469d854c0708b98a73a0fdaf24d3ec9dd2452ca
Romi <romesa.arshad.baloch@gmail.com>
2024-12-03 03:01:57 +0500

    Add local and git path for the main.jsx file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GreakStack/', // This is the key to make sure production builds are correct
 })
